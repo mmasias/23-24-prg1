ac39305a6589d3e1e8a040b062ceeef5fee9ff1b
Maura Martinez <137115639+Martinez-rosa@users.noreply.github.com>
2023-10-02 15:33:01 +0200

    Update DevolverCambio.java

--- entregas/Reto001/DevolverCambio.java
+++ entregas/Reto001/DevolverCambio.java
@@ -1,47 +1,30 @@
-import java.util.Scanner;
-
 public class DevolverCambio{
 
-    public static void main(String[] args){
-        
-        Scanner sc = new Scanner(System.in);
+public static void main(String[] args){
+    
+    
+        int amountPaid = 400;
+        int productPrice = 232;
+        int change = amountPaid - productPrice;
 
-        System.out.println("Indica el monto a pagar: ");
-        int aPagar = sc.nextInt();
-        
-        System.out.println("Indique el monto a entregar: ");
-        int montoDado = sc.nextInt();
-         
-        if(montoDado < aPagar){
-            System.out.println("El dinero ingresado es insuficiente");
-        }
-
-        int devolver = montoDado - aPagar;
-        System.out.println("Su cambio es :  " + devolver + " " + "euros");
-       
-        while(devolver!=0){
             
-            int[] billetesMonedas = {100, 50, 20, 10, 5, 2, 1};
-            String[] nombreBilletesMonedas = {"billetes de 1100", "billetes de 50", "billetes de 20", "billetes de 10", "billetes de 5", "monedas de 2", "monedas de 1"};
-            System.out.println("--------------------------------------------");
+        int hundredBills = change / 100;
+        int fiftyBills = (change % 100) / 50;
+        int twentyBills = ((change % 100) % 50) / 20;
+        int tenBills = (((change % 100) % 50) % 20) / 10;
+        int fiveBills = ((((change % 100) % 50) % 20) % 10) / 5;
+        int twoCoins = (((((change % 100) % 50) % 20) % 10) % 5) / 2;
+        int oneCoins = ((((((change % 100) % 50) % 20) % 10) % 5) % 2) / 1;
 
-            for(int i = 0; i < billetesMonedas.length; i++){
-                int cantidad = devolver/billetesMonedas[i];
-                devolver %= billetesMonedas[i];
-                
-                System.out.println("Cantidad de " + nombreBilletesMonedas[i]+ ": " + cantidad);
-            }
         
-       
-
-        }
-
+        System.out.println("Change: " + change);
+        System.out.println("100 Bills: " + hundredBills);
+        System.out.println("50 Bills: " + fiftyBills);
+        System.out.println("20 Bills: " + twentyBills);
+        System.out.println("10 Bills: " + tenBills);
+        System.out.println("5 Bills: " + fiveBills);
+        System.out.println("2 Coins: " + twoCoins);
+        System.out.println("1 Coins: " + oneCoins);
+        
     }
 }
-
-       
-            
-
-
-       
-
