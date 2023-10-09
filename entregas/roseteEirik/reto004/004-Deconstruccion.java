e28414176f3c901fb9777bcf50dfe41b07140d78
eirik.rosete <eirik.rosete@gmail.com>
2023-10-09 16:12:48 +0200

    Retos: 001, 002, 003, 004, 005

---
+++ entregas/roseteEirik/reto004/004-Deconstruccion.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+class Deconstruccion
+{
+    public static void main(String[] args)
+    {
+        try (Scanner scanner = new Scanner(System.in))
+        {
+            System.out.println("\nInserte la primera cifra de tres digitos: ");
+            int numberOne = scanner.nextInt();
+
+            System.out.println("\nInserte la segunda cifra de tres digitos: ");
+            int numberTwo = scanner.nextInt();
+
+            System.out.println("\nInserte la tercera cifra de tres digitos: ");
+            int numberThree = scanner.nextInt();
+
+            final int aboveThreeDigits = 1000;
+            int digitOne = numberOne / 100;
+            int residueNumberTwo = numberTwo % 100;
+            int digitTwo = residueNumberTwo / 10;
+            int digitThree = numberThree % 10;
+
+            System.out.println(numberOne >= aboveThreeDigits ? "\nError: El numero " + numberOne + " es mayor de tres cifras" : numberTwo >= aboveThreeDigits ? "\nError: El numero " + numberTwo + " es mayor de tres cifras" : numberThree >= aboveThreeDigits ? "\nError: El numero " + numberThree + " es mayor de tres cifras" : "\nNumero: " + digitOne + "" + digitTwo + "" + digitThree);
+
+        }
+    }
+}
---
+++ entregas/roseteEirik/reto005/005-NumeroMayor.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class NumeroMayor
+{
+    public static void main(String[] args)
+    {
+        try (Scanner scanner = new Scanner(System.in))
+        {
+            System.out.println("\nIngrese el primer numero: ");
+            int numberOne = scanner.nextInt();
+            System.out.println("\nIngrese el segundo numero: ");
+            int numberTwo = scanner.nextInt();
+
+            System.out.println("\nEl numero mayor es:");
+            System.out.println(numberOne > numberTwo ? numberOne : numberTwo);
+        }
+    }
+}
