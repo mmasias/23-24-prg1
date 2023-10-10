aa4a6b3fedf0572d620149e34b332439275c5b3d
xImRaul <144956120+xImRaul@users.noreply.github.com>
2023-10-10 13:40:41 +0200

    Update reto1.java

--- entregas/garciaRaul/reto-001/reto1.java
+++ entregas/garciaRaul/reto-001/reto1.java
@@ -1,36 +1,45 @@
-public class reto1 {
-    public static void main(String[] args) {
-        System.out.println("Debes pagar 232 euros");
-        System.out.println("He entregado 400 euros");
-        System.out.println("-----------------------------------");
+public class reto1{
+public static void main(String[] args) {
 
-        int cambio = 168;
+int billetes100= 0;
+int billetes50= 0;
+int billetes20= 0;
+int billetes10= 0;
+int billetes5= 0;
+int monedas2= 0;
+int monedas1= 0;
 
-        int billetesde100 = cambio / 100;
-        cambio %= 100;
+int cantidad_pagar = 400;
+int cantidad_entregada = 1432;
 
-        int billetesde50 = cambio / 50;
-        cambio %= 50;
+System.out.println("Debes pagar: " + cantidad_pagar);
+System.out.println("Has pagado: " + cantidad_entregada);
+System.out.println("-------------------------");
 
-        int billetesde20 = cambio / 20;
-        cambio %= 20;
+int cantidad_devolver = cantidad_entregada-cantidad_pagar;
 
-        int billetesde10 = cambio / 10;
-        cambio %= 10;
+billetes100 = cantidad_devolver/100;
+cantidad_devolver = cantidad_devolver-(100*billetes100);
+billetes50 = cantidad_devolver/50;
+cantidad_devolver = cantidad_devolver-(50*billetes50);
+billetes20 = cantidad_devolver/20;
+cantidad_devolver = cantidad_devolver-(20*billetes20);
+billetes10 = cantidad_devolver/10;
+cantidad_devolver = cantidad_devolver-(10*billetes10);
+billetes5 = cantidad_devolver/5;
+cantidad_devolver = cantidad_devolver-(5*billetes5);
+monedas2 = cantidad_devolver/2;
+cantidad_devolver = cantidad_devolver-(2*monedas2);
+monedas1 = cantidad_devolver/1;
+cantidad_devolver = cantidad_devolver-(1*monedas1);
 
-        int billetesde5 = cambio / 5;
-        cambio %= 5;
+System.out.println(billetes100 + " billete(s) de 100€");
+System.out.println(billetes50 + " billete(s) de 50€");
+System.out.println(billetes20 + " billete(s) de 20€");
+System.out.println(billetes10 + " billete(s) de 10€");
+System.out.println(billetes5 + " billete(s) de 5€");
+System.out.println(monedas2 + " moneda(s) de 2€");
+System.out.println(monedas1 + " moneda(s) de 1€");
 
-        int billetesde2 = cambio / 2;
-        cambio %= 2;
-        int billetesde1 = cambio;
-
-        System.out.println(billetesde100 + "Billete(s) de 100€");
-        System.out.println(billetesde50 + "Billete(s) de 50€");
-        System.out.println(billetesde20 + "Billete(s) de 20€");
-        System.out.println(billetesde10 + "Biletes(s) de 10€");
-        System.out.println(billetesde5 + "Billete(s) de 5€");
-        System.out.println(billetesde2 + "Billete(s) de 2€");
-        System.out.println(billetesde1 + "Billete(s) de 1€");
-    }
+}
 }
