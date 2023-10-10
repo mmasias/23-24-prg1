0444e44082606a898835d463556241fe35b9c749
marcosgutierrez6 <144948806+marcosgutierrez6@users.noreply.github.com>
2023-10-10 13:22:16 +0200

    Bucles quitado y construccion del primer numero
    faltan los otros 2

--- entregas/gutierrezMarcos/reto-004/Deconstruccion.java
+++ entregas/gutierrezMarcos/reto-004/Deconstruccion.java
@@ -7,22 +7,13 @@ public class decostruccion{
 
         System.out.println("Introduce un 1er numero de 3 digitos");
         int num1 = num.nextInt();
-        while(num1<100 || num1>999){
-            System.out.println("Introduce un 1er numero de 3 digitos");
-            num1 = num.nextInt();
-        }
+
         System.out.println("Introduce un 2º numero de 3 digitos");
         int num2 = num.nextInt();
-        while(num2<100 || num2>999){
-            System.out.println("Introduce un 2º numero de 3 digitos");
-            num2 = num.nextInt();
-        }
+
         System.out.println("Introduce un 3er numero de 3 digitos");
         int num3 = num.nextInt();
-        while(num3<100 || num3>999){
-            System.out.println("Introduce un 3er numero de 3 digitos");
-            num3 = num.nextInt();
-        }
+
 
         int numFinal;
 
