69baa07cba7b65774b5fb1be91fdc6aaa4351e75
marcosgutierrez6 <144948806+marcosgutierrez6@users.noreply.github.com>
2023-10-10 13:18:28 +0200

    Update devolverCambioMejorado.java
    Falta agregar que si no hay billetes el mensaje no aparezca(sin usar condicionales)

--- entregas/gutierrezMarcos/reto-003/devolverCambioMejorado.java
+++ entregas/gutierrezMarcos/reto-003/devolverCambioMejorado.java
@@ -1,1 +1,46 @@
+import java.util.Scanner;
 
+public class cambio {
+    private static Scanner money;
+
+    public static void main(String[] arg) {
+        money = new Scanner(System.in);
+
+        System.out.println("Cuantos euros debe?");
+        int rMoney = money.nextInt();
+
+        System.out.println("Cuantos euros entrega?");
+        int eMoney = money.nextInt();
+
+
+        int cambio = eMoney - rMoney;
+
+        int billetesDe100 = cambio / 100;
+        cambio %= 100;
+
+        int billetesDe50 = cambio / 50;
+        cambio %= 50;
+
+        int billetesDe20 = cambio / 20;
+        cambio %= 20;
+
+        int billetesDe10 = cambio / 10;
+        cambio %= 10;
+
+        int billetesDe5 = cambio / 5;
+        cambio %= 5;
+
+        int billetesDe2 = cambio / 2;
+        cambio %= 2;
+
+        int billetesDe1 = cambio;
+
+        System.out.println(billetesDe100+" billetes de 100€");
+        System.out.println(billetesDe50+" billetes de 50€");
+        System.out.println(billetesDe20+" billetes de 20€");
+        System.out.println(billetesDe10+" billetes de 10€");
+        System.out.println(billetesDe5+" billetes de 5€");
+        System.out.println(billetesDe2+" billetes de 2€");
+        System.out.println(billetesDe1+" billetes de 1€");
+    }
+}
