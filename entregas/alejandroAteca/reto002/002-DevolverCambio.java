f633a6cd00b4e5d5ba68d5dee86849aae49c33a1
alejandroateca <atecaalejandro8@gmail.com>
2023-10-10 19:22:54 +0200

    reto 2 (formateado)

--- entregas/alejandroAteca/reto002/002-DevolverCambio.java
+++ entregas/alejandroAteca/reto002/002-DevolverCambio.java
@@ -1,13 +1,13 @@
 class ChangeCalculator2 {
-    public static void main(String [] args) {
 
+  public static void main(String[] args) {
     int paid = 400;
     int shellPrice = 232;
-    
+
     System.out.println("Debe pagar " + shellPrice + " €");
 
     System.out.println("Ha entregado " + paid + " €");
-    
+
     int billsNumber;
     int nowWorking;
     int change = paid - shellPrice;
@@ -46,7 +46,5 @@ class ChangeCalculator2 {
     billsNumber = (change - (change % nowWorking)) / nowWorking;
     change = change - (nowWorking * billsNumber);
     System.out.println(billsNumber > 0 ? billsNumber + " monedas de " + nowWorking + "€" : "");
-
-      }
-    
+  }
 }
