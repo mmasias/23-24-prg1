76510c8f3bf061158d713801625de179011082d9
marcosgutierrez6 <144948806+marcosgutierrez6@users.noreply.github.com>
2023-10-10 13:10:39 +0200

    Update devolverCambio.java

--- entregas/gutierrezMarcos/reto-002/devolverCambio.java
+++ entregas/gutierrezMarcos/reto-002/devolverCambio.java
@@ -1,1 +1,33 @@
+public class devolverCambio {
+    public static void main(String[] arg) {
+        
+        int cambio = 168;
 
+        int billetesDe100 = cambio / 100;
+        cambio %= 100;
+
+        int billetesDe50 = cambio / 50;
+        cambio %= 50;
+
+        int billetesDe20 = cambio / 20;
+        cambio %= 20;
+
+        int billetesDe10 = cambio / 10;
+        cambio %= 10;
+
+        int billetesDe5 = cambio / 5;
+        cambio %= 5;
+
+        int billetesDe2 = cambio / 2;
+        cambio %= 2;
+
+        int billetesDe1 = cambio;
+
+        System.out.println(billetesDe100+" billetes de 100€");
+        System.out.println(billetesDe50+" billetes de 50€");
+        System.out.println(billetesDe10+" billetes de 10€");
+        System.out.println(billetesDe5+" billetes de 5€");
+        System.out.println(billetesDe2+" billetes de 2€");
+        System.out.println(billetesDe1+" billetes de 1€");
+    }
+}
