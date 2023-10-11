332c59081fa3abefcf71b24b22689ac25fa2e92f
Rub�n Tresgallo Balb�n <ruben.tresgallo@unealum.local>
2023-10-11 13:26:43 +0200

    guardado retos

---
+++ entregas/tresgalloRuben/Reto4/reto4.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class reto4 {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+      
+        System.out.print("Ingrese el primer numero de tres cifras: ");
+        int numeroA = scanner.nextInt();
+
+        System.out.print("Ingrese el segundo numero de tres cifras: ");
+        int numeroB = scanner.nextInt();
+
+        System.out.print("Ingrese el tercer numero de tres cifras: ");
+        int numeroC = scanner.nextInt();
+
+    
+        int cifra1 = numeroA / 100; 
+        int cifra2 = (numeroB / 10) % 10; 
+        int cifra3 = numeroC % 10; 
+
+    
+        int nuevoNumero = cifra1 * 100 + cifra2 * 10 + cifra3;
+
+    
+        System.out.println("El nuevo numero resultante es: " + nuevoNumero);
+    }
+}
