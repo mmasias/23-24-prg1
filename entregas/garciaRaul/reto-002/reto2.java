ac9a95bb0c29d9e69ba1f85c6f97a494cee98e15
xImRaul <144956120+xImRaul@users.noreply.github.com>
2023-10-11 11:09:07 +0200

    Update reto2.java

--- entregas/garciaRaul/reto-002/reto2.java
+++ entregas/garciaRaul/reto-002/reto2.java
@@ -1,4 +1,5 @@
-public class reto2{
+public class reto2 {
+
 public static void main(String[] args) {
 
 int billetes100= 0;
@@ -9,14 +10,14 @@ int billetes5= 0;
 int monedas2= 0;
 int monedas1= 0;
 
-int cantidad_pagar = 400;
-int cantidad_entregada = 15532;
+int cantidadpagar = 400;
+int cantidadentregada = 1432;
 
-System.out.println("Debes pagar: " + cantidad_pagar);
-System.out.println("Has entregado: " + cantidad_entregada);
+System.out.println("Debes pagar: " + cantidadpagar);
+System.out.println("Has pagado: " + cantidadentregada);
 System.out.println("-------------------------");
 
-int cantidad_devolver = cantidad_entregada-cantidad_pagar;
+int cantidad_devolver = cantidadentregada-cantidadpagar;
 
 billetes100 = cantidad_devolver/100;
 cantidad_devolver = cantidad_devolver-(100*billetes100);
@@ -33,13 +34,23 @@ cantidad_devolver = cantidad_devolver-(2*monedas2);
 monedas1 = cantidad_devolver/1;
 cantidad_devolver = cantidad_devolver-(1*monedas1);
 
-System.out.println(billetes100 + " billete(s) de 100€");
-System.out.println(billetes50 + " billete(s) de 50€");
-System.out.println(billetes20 + " billete(s) de 20€");
-System.out.println(billetes10 + " billete(s) de 10€");
-System.out.println(billetes5 + " billete(s) de 5€");
-System.out.println(monedas2 + " moneda(s) de 2€");
-System.out.println(monedas1 + " moneda(s) de 1€");
+String mensaje100 = billetes100 > 0 ? billetes100 + " billete(s) de 100€\n" : "";
+String mensaje50 = billetes50 > 0 ? billetes50 + " billete(s) de 50€\n" : "";
+String mensaje20 = billetes20 > 0 ? billetes20 + " billete(s) de 20€\n" : "";
+String mensaje10 = billetes10 > 0 ? billetes10 + " billete(s) de 10€\n" : "";
+String mensaje5 = billetes5 > 0 ? billetes5 + " billete(s) de 5€\n" : "";
+String mensaje2 = monedas2 > 0 ? monedas2 + " moneda(s) de 2€\n" : "";
+String mensaje1 = monedas1 > 0 ? monedas1 + " moneda(s) de 1€\n" : "";
+
+System.out.print(mensaje100);
+System.out.print(mensaje50);
+System.out.print(mensaje20);
+System.out.print(mensaje10);
+System.out.print(mensaje5);
+System.out.print(mensaje2);
+System.out.print(mensaje1);
 
 }
+
 }
+    
