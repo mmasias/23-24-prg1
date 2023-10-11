153ce9f4a26dfd33f4d862c3da9b81b3688713bb
xImRaul <144956120+xImRaul@users.noreply.github.com>
2023-10-11 13:23:47 +0200

    sin terminar reto5.java

--- entregas/garciaRaul/reto-005/reto5.java
+++ entregas/garciaRaul/reto-005/reto5.java
@@ -1,1 +1,18 @@
+import java.util.Scanner;
+public class reto5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Dime un primer numero entero");
+        int numero1 = scanner.nextInt();
+        System.out.println("Dime un segundo numero entero");
+        int numero2 = scanner.nextInt();
 
+        String mayormenor = numero1 > numero2 ? "El numero mayor es " + numero1 : "El numero mayor es " + numero2;
+        System.out.println(mayormenor);
+
+
+
+        
+    }
+    
+}
