e68d6b3a399b09c345430d80d367a191a52a4511
Oscar Santamaria <114479802+Oscarsantasanchez@users.noreply.github.com>
2023-10-12 11:19:16 +0200

    Update DevolverCambio.java

--- entregas/santamariaOscar/reto001/DevolverCambio.java
+++ entregas/santamariaOscar/reto001/DevolverCambio.java
@@ -1,7 +1,7 @@
 
 public class DevolverCambio {
     public static void main(String[] args) {
-        int Billete100 = 100;
+        int billete100 = 100;
         int billete50 = 50;
         int billete20 = 20;
         int billete10 = 10;
@@ -9,35 +9,32 @@ public class DevolverCambio {
         int moneda2 = 2;
         int moneda1 = 1;
 
-        int cantidadAPagar = 150, cantidadEntregada=200, cambio;
+        int cantidadAPagar = 150, cantidadEntregada = 200, cambio;
         cambio = cantidadEntregada - cantidadAPagar;
 
-        int billete100 = cambio / 100;
-        cambio = cambio % Billete100;
-        billete50 = cambio / 50;
+        int billetes100 = cambio / billete100;
+        cambio = cambio % billete100;
+        int billetes50 = cambio / billete50;
         cambio = cambio % billete50;
-        billete20 = cambio / 20;
+        int billetes20 = cambio / billete20;
         cambio = cambio % billete20;
-        billete10 = cambio / 10;
+        int billetes10 = cambio / billete10;
         cambio = cambio % billete10;
-        billete5 = cambio / 5;
+        int billetes5 = cambio / billete5;
         cambio = cambio % billete5;
-        moneda2 = cambio / 2;
+        int monedas2 = cambio / moneda2;
         cambio = cambio % moneda2;
-        moneda1 = cambio / 1;
+        int monedas1 = cambio / moneda1;
         cambio = cambio % moneda1;
 
 
-        System.out.println("El cambio es: " + cambio);
-        System.out.println("------------------------");
-        System.out.println("Billetes de 100: " + billete100);
-        System.out.println("Billetes de 50: " + billete50);
-        System.out.println("Billetes de 20: " + billete20);
-        System.out.println("Billetes de 10: " + billete10);
-        System.out.println("Billetes de 5: " + billete5);
-        System.out.println("Monedas de 2: " + moneda2);
-        System.out.println("Monedas de 1: " + moneda1);
-
+        System.out.println("Billetes de 100: " + billetes100);
+        System.out.println("Billetes de 50: " + billetes50);
+        System.out.println("Billetes de 20: " + billetes20);
+        System.out.println("Billetes de 10: " + billetes10);
+        System.out.println("Billetes de 5: " + billetes5);
+        System.out.println("Monedas de 2: " + monedas2);
+        System.out.println("Monedas de 1: " + monedas1);
 
     }
 }
