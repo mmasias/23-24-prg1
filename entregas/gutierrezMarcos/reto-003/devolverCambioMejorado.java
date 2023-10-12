4dc8a19ee396f232d106ad442d657db0e903417c
marcosgutierrez6 <144948806+marcosgutierrez6@users.noreply.github.com>
2023-10-12 16:11:25 +0200

    Update devolverCambioMejorado.java

--- entregas/gutierrezMarcos/reto-003/devolverCambioMejorado.java
+++ entregas/gutierrezMarcos/reto-003/devolverCambioMejorado.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class devolverCambioMejorado {
+public class DevolverCambioMejorado {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
