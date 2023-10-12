f68c0b3a767489654ef536c25ce38674ae102579
marcosgutierrez6 <144948806+marcosgutierrez6@users.noreply.github.com>
2023-10-12 15:30:44 +0200

    aplicacion de los operadores ternarios

--- entregas/gutierrezMarcos/reto-002/devolverCambio.java
+++ entregas/gutierrezMarcos/reto-002/devolverCambio.java
@@ -1,33 +1,36 @@
 public class devolverCambio {
-    public static void main(String[] arg) {
-        
-        int cambio = 168;
+    public static void main(String[] args) {
+        int precio = 1307;
+        int cantidad = 2000;
+        int cambio = cantidad - precio;
+        System.out.println("El cambio es: " + cambio);
 
-        int billetesDe100 = cambio / 100;
-        cambio %= 100;
+        int billete100 = cambio / 100;
+        cambio = cambio % 100;
 
-        int billetesDe50 = cambio / 50;
-        cambio %= 50;
+        int billete50 = cambio / 50;
+        cambio = cambio % 50;
 
-        int billetesDe20 = cambio / 20;
-        cambio %= 20;
+        int billete20 = cambio / 20;
+        cambio = cambio % 20;
 
-        int billetesDe10 = cambio / 10;
-        cambio %= 10;
+        int billete10 = cambio / 10;
+        cambio = cambio % 10;
 
-        int billetesDe5 = cambio / 5;
-        cambio %= 5;
+        int billete5 = cambio / 5;
+        cambio = cambio % 5;
 
-        int billetesDe2 = cambio / 2;
-        cambio %= 2;
+        int moneda2 = cambio / 2;
+        cambio = cambio % 2;
 
-        int billetesDe1 = cambio;
+        int moneda1 = cambio / 1;
+        cambio = cambio % 1;
 
-        System.out.println(billetesDe100+" billetes de 100€");
-        System.out.println(billetesDe50+" billetes de 50€");
-        System.out.println(billetesDe10+" billetes de 10€");
-        System.out.println(billetesDe5+" billetes de 5€");
-        System.out.println(billetesDe2+" billetes de 2€");
-        System.out.println(billetesDe1+" billetes de 1€");
+        System.out.println(billete50 > 0 ? "Billetes de 50 euros: " + billete50 : "");
+        System.out.println(billete20 > 0 ? "Billetes de 20 euros: " + billete20 : "");
+        System.out.println(billete10 > 0 ? "Billetes de 10 euros: " + billete10 : "");
+        System.out.println(billete5 > 0 ? "Billetes de 5 euros: " + billete5 : "");
+        System.out.println(moneda2 > 0 ? "Monedas de 2 euros: " + moneda2 : "");
+        System.out.println(moneda1 > 0 ? "Monedas de 1 euros: " + moneda1 : "");
     }
 }
