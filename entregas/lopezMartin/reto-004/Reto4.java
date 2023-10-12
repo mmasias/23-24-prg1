b83cbf14269ff39ed4afb791db71e7478ea4dc6b
martinlopez7 <144948913+martinlopez7@users.noreply.github.com>
2023-10-12 15:55:51 +0200

    Update Reto4.java

--- entregas/lopezMartin/reto-004/Reto4.java
+++ entregas/lopezMartin/reto-004/Reto4.java
@@ -7,19 +7,19 @@ class Reto4 {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Introduce un primer numero de 3 cifras");
-        int num1 = scanner.nextInt();
+        int numero1 = scanner.nextInt();
 
         System.out.println("Introduce un segundo numero de 3 cifras");
-        int num2 = scanner.nextInt();
+        int numero2 = scanner.nextInt();
 
         System.out.println("Introduce un tercer numero de 3 cifras");
-        int num3 = scanner.nextInt();
+        int numero3 = scanner.nextInt();
 
-        int digito1 = num1>=100 && num1<= 999 ? num1/100 : null ;
-        int digito2_1 = num2>=100 && num2<= 999 ? num2%100 : null;
+        int digito1 = numero1>=100 && numero1<= 999 ? numero1/100 : null ;
+        int digito2_1 = numero2>=100 && numero2<= 999 ? numero2%100 : null;
         double digito2_2 = digito2_1* 0.1;
         int digito2Final = (int) digito2_2;
-        int digito3 = num3>= 100 && num3 <= 999 ? num3%10 : null;
+        int digito3 = numero3>= 100 && numero3 <= 999 ? numero3%10 : null;
 
         int numeroFinal = digito1*100 + digito2Final*10 + digito3;
 
