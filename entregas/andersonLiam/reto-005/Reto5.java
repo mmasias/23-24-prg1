2e60ab1289622fab54186cc2b10018cc4338b52d
liamanderson873 <liam.anderson@alumnos.uneatlantico>
2023-10-12 20:44:49 +0200

    nombres carpetas

---
+++ entregas/andersonLiam/reto-005/Reto5.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Reto5 {
+
+    public static void main(String[] args) {
+       
+        Scanner scanner = new Scanner(System.in);
+
+        
+        System.out.print("Ingrese el primer número: ");
+        int numero1 = scanner.nextInt();
+
+       
+        System.out.print("Ingrese el segundo número: ");
+        int numero2 = scanner.nextInt();
+
+        
+        int mayor = numero1 > numero2 ? numero1 : numero2;
+
+       
+        System.out.println("El mayor de los dos números es: " + mayor);
+
+        
+        scanner.close();
+    }
+}
