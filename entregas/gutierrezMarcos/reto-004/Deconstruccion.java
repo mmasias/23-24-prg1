6e9a2e56cb83da00644ee25c9e7771e94d8da4bc
marcosgutierrez6 <144948806+marcosgutierrez6@users.noreply.github.com>
2023-10-12 16:11:06 +0200

    Update Deconstruccion.java

--- entregas/gutierrezMarcos/reto-004/Deconstruccion.java
+++ entregas/gutierrezMarcos/reto-004/Deconstruccion.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class decostruccion {
+public class Decostruccion {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
