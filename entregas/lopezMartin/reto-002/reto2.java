e3b40d4205fc4aa4b6a9782cdaef3757eeab5a87
martinlopez7 <144948913+martinlopez7@users.noreply.github.com>
2023-10-12 15:18:43 +0200

    reto 2 corregido

--- entregas/lopezMartin/reto-002/reto2.java
+++ entregas/lopezMartin/reto-002/reto2.java
@@ -1,5 +1,4 @@
-public class reto2 {
-    
+class Reto2{
     public static void main(String[] args) {
 
         int billetes100= 0;
@@ -9,36 +8,36 @@ public class reto2 {
         int billetes5= 0;
         int monedas2= 0;
         int monedas1= 0;
-        
-        int cantidad_pagar = 400;
-        int cantidad_entregada = 1432;
 
-        System.out.println("Debes pagar: " + cantidad_pagar);
-        System.out.println("Has pagado: " + cantidad_entregada);
+        int cantidadPagar = 400;
+        int cantidadEntregada = 1432;
+
+        System.out.println("Debes pagar: " + cantidadPagar);
+        System.out.println("Has pagado: " + cantidadEntregada);
         System.out.println("-------------------------");
 
-        int cantidad_devolver = cantidad_entregada-cantidad_pagar;
+        int cantidadDevolver = cantidadEntregada-cantidadPagar;
 
-        billetes100 = cantidad_devolver/100;
-        cantidad_devolver = cantidad_devolver-(100*billetes100);
-        billetes50 = cantidad_devolver/50;
-        cantidad_devolver = cantidad_devolver-(50*billetes50);
-        billetes20 = cantidad_devolver/20;
-        cantidad_devolver = cantidad_devolver-(20*billetes20);
-        billetes10 = cantidad_devolver/10;
-        cantidad_devolver = cantidad_devolver-(10*billetes10);
-        billetes5 = cantidad_devolver/5;
-        cantidad_devolver = cantidad_devolver-(5*billetes5);
-        monedas2 = cantidad_devolver/2;
-        cantidad_devolver = cantidad_devolver-(2*monedas2);
-        monedas1 = cantidad_devolver/1;
-        cantidad_devolver = cantidad_devolver-(1*monedas1);
-        
+        billetes100 = cantidadDevolver/100;
+        cantidadDevolver = cantidadDevolver-(100*billetes100);
+        billetes50 = cantidadDevolver/50;
+        cantidadDevolver = cantidadDevolver-(50*billetes50);
+        billetes20 = cantidadDevolver/20;
+        cantidadDevolver = cantidadDevolver-(20*billetes20);
+        billetes10 = cantidadDevolver/10;
+        cantidadDevolver = cantidadDevolver-(10*billetes10);
+        billetes5 = cantidadDevolver/5;
+        cantidadDevolver = cantidadDevolver-(5*billetes5);
+        monedas2 = cantidadDevolver/2;
+        cantidadDevolver = cantidadDevolver-(2*monedas2);
+        monedas1 = cantidadDevolver/1;
+        cantidadDevolver = cantidadDevolver-(1*monedas1);
+
         String mensaje100 = billetes100 > 0 ? billetes100 + " billete(s) de 100€\n" : "";
         String mensaje50 = billetes50 > 0 ? billetes50 + " billete(s) de 50€\n" : "";
         String mensaje20 = billetes20 > 0 ? billetes20 + " billete(s) de 20€\n" : "";
         String mensaje10 = billetes10 > 0 ? billetes10 + " billete(s) de 10€\n" : "";
-        String mensaje5 = billetes5 > 0 ? billetes5 + " billete(s) de 5€\n" : "";     
+        String mensaje5 = billetes5 > 0 ? billetes5 + " billete(s) de 5€\n" : "";
         String mensaje2 = monedas2 > 0 ? monedas2 + " moneda(s) de 2€\n" : "";
         String mensaje1 = monedas1 > 0 ? monedas1 + " moneda(s) de 1€\n" : "";
 
@@ -51,5 +50,4 @@ public class reto2 {
         System.out.print(mensaje1);
 
     }
-
 }
