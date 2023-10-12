5f24eb7a27650186c3e009b46fc8c9bf4b6ace0f
marcosgutierrez6 <144948806+marcosgutierrez6@users.noreply.github.com>
2023-10-12 16:10:55 +0200

    Update numeroMayor.java

--- entregas/gutierrezMarcos/reto-005/numeroMayor.java
+++ entregas/gutierrezMarcos/reto-005/numeroMayor.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class numeroMayor {
+public class NumeroMayor {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
