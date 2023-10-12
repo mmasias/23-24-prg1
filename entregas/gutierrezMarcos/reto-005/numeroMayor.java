e3579e77cb825b09e83b4c58a40b2c4c3f463c47
marcosgutierrez6 <144948806+marcosgutierrez6@users.noreply.github.com>
2023-10-12 15:33:34 +0200

    Update numeroMayor.java

--- entregas/gutierrezMarcos/reto-005/numeroMayor.java
+++ entregas/gutierrezMarcos/reto-005/numeroMayor.java
@@ -1,1 +1,21 @@
+import java.util.Scanner;
 
+public class numeroMayor {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.print("Ingrese el primer número entero: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Ingrese el segundo número entero: ");
+        int num2 = scanner.nextInt();
+
+
+        int mayor = (num1 > num2) ? num1 : num2;
+
+       
+        System.out.println("El número mayor es: " + mayor);
+    }
+}
