fd10f0e654a1065f4d899e7675d60b121392cd11
alejandrofondo <jose.fondo@alumnos.uneatlantico.es>
2023-10-12 11:20:26 +0200

    Cambio nombre carpeta e indentar V1.1

---
+++ entregas/fondoJose/reto-002/CambioExtendido.java
@@ -0,0 +1,48 @@
+class CambioExtendido {
+    public static void main(String[] args) {
+
+        int entrega = 400;
+        int debe = 232;
+        int tipoBillete;
+        int billete;
+        int porDevolver = entrega - debe;
+        String mensaje = "";
+
+        tipoBillete = 100;
+        billete = porDevolver / tipoBillete;
+        porDevolver = porDevolver % 100;
+        mensaje = mensaje + ((billete > 0) ? billete + " billetes de " + tipoBillete + "\n": "");
+
+        tipoBillete = 50;
+        billete = porDevolver / tipoBillete;
+        porDevolver = porDevolver % 50;
+        mensaje = mensaje + ((billete > 0) ? billete + " billetes de " + tipoBillete + "\n": "");
+
+        tipoBillete = 20;
+        billete = porDevolver / tipoBillete;
+        porDevolver = porDevolver % 20;
+        mensaje = mensaje + ((billete > 0) ? billete + " billetes de " + tipoBillete + "\n": "");
+
+        tipoBillete = 10;
+        billete = porDevolver / tipoBillete;
+        porDevolver = porDevolver % 10;
+        mensaje = mensaje + ((billete > 0) ? billete + " billetes de " + tipoBillete + "\n": "");
+
+        tipoBillete = 5;
+        billete = porDevolver / tipoBillete;
+        porDevolver = porDevolver % 5;
+        mensaje = mensaje + ((billete > 0) ? billete + " billetes de " + tipoBillete + "\n": "");
+
+        tipoBillete = 2;
+        billete = porDevolver / tipoBillete;
+        porDevolver = porDevolver % 2;
+        mensaje = mensaje + ((billete > 0) ? billete + " billetes de " + tipoBillete + "\n": "");
+
+        tipoBillete = 1;
+        billete = porDevolver / tipoBillete;
+        porDevolver = porDevolver % 1;
+        mensaje = mensaje + ((billete > 0) ? billete + " billetes de " + tipoBillete + "\n": "");
+
+        System.out.println("La cantidad de billetes a devolver son:" + "\n" + mensaje);
+    }
+}
