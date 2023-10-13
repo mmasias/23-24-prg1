b6b1ad8b367bf7b1949581c4af8a3c2a9d864ae4
Camila Lesly Palma <90413743+clpalma2003@users.noreply.github.com>
2023-10-13 10:40:15 -0500

    Update DevolverCambio.java

--- entregas/palmaCamila/reto001/DevolverCambio.java
+++ entregas/palmaCamila/reto001/DevolverCambio.java
@@ -1,42 +1,42 @@
-public class DevolverCambio {
+class DevolverCambio {
     public static void main(String[] args) {
 
         int price = 232;
-        int givenMoney  = 400;
+        int receivedMoney  = 400;
 
         int change = givenMoney  - price;
 
-        int oneHundredBill = change / 100;
+        int numberOfHundredDollarBill = change / 100;
         change %= 100;
 
-        int fifBill = change / 50;
+        int numberOfFiftyDollarBill = change / 50;
         change %= 50;
 
-        int twtBill = change / 20;
+        int numberOfTwentyDollarBill = change / 20;
         change %= 20;
 
-        int tenBill = change / 10;
+        int numberOfTenDollarDollarBill = change / 10;
         change %= 10;
 
-        int fiveBill = change / 5;
+        int numberOfFiveDollarBill = change / 5;
         change %= 5;
 
-        int twoCoin = change / 2;
+        int numberOfTwoCoin = change / 2;
         change %= 2;
 
-        int oneCoin = change / 1;
+        int numberOfOneCoin = change / 1;
         change %= 1;
 
         System.out.println("Debe pagar :" + price);
         System.out.println("Ha entregado :" + givenMoney );
 
-        System.out.println(oneHundredBill + " billete(s) de 100€ ");
-        System.out.println(fifBill + " billete(s) de 50€ ");
-        System.out.println(twtBill + " billete(s) de 20€ ");
-        System.out.println(tenBill + " billete(s) de 10€: ");
-        System.out.println(fiveBill + " billete(s) de 5€: " );
-        System.out.println(twoCoin + " moneda(s) de 2€: ");
-        System.out.println(oneCoin + " moneda(s) de 1€: ");
+        System.out.println(numberOfHundredDollarBill + " billete(s) de 100€ ");
+        System.out.println(numberOfFiftyDollarBill + " billete(s) de 50€ ");
+        System.out.println(numberOfTwentyDollarBill + " billete(s) de 20€ ");
+        System.out.println(numberOfTenDollarDollarBill + " billete(s) de 10€: ");
+        System.out.println(numberOfFiveDollarBill + " billete(s) de 5€: " );
+        System.out.println(numberOfTwoCoin + " moneda(s) de 2€: ");
+        System.out.println(numberOfOneCoin + " moneda(s) de 1€: ");
 
     }
 }
