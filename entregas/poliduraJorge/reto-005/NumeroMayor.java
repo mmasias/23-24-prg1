4d80096d65d13b706076b1fc62e64ee2c3ef5401
JorgePolidura123 <144842516+JorgePolidura123@users.noreply.github.com>
2023-10-14 23:37:03 +0200

    Update NumeroMayor.java

--- entregas/poliduraJorge/reto-005/NumeroMayor.java
+++ entregas/poliduraJorge/reto-005/NumeroMayor.java
@@ -1,1 +1,19 @@
+import java.util.Scanner;
+
+public class Reto005 {
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Introduce un numero entero:");
+        int num1=sc.nextInt();
+        System.out.println("Introduce otro numero entero:");
+        int num2=sc.nextInt();
+
+        String mayor_menor=num1>num2 ? "El mayor es " + num1 : "El mayor es " + num2;
+
+        System.out.println(mayor_menor);
+
+    }
+}
 
