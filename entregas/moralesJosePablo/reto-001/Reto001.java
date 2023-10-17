e5298cb8d512b9592c46dfbec3223eddedd75f4a
JPMorales04 <jpmorales.epic@gmail.com>
2023-10-17 20:32:37 +0200

    Reto006

--- entregas/moralesJosePablo/reto-001/Reto001.java
+++ entregas/moralesJosePablo/reto-001/Reto001.java
@@ -6,21 +6,20 @@ public class Reto001 {
         int cambio = montoEntregado - montoAPagar;
         System.out.println("Cambio a devolver: " + cambio + " euros");
 
-        int[] denominaciones = { 100, 50, 20, 10, 5, 2, 1 };
+        int[] BilletesyMonedas = { 100, 50, 20, 10, 5, 2, 1 };
 
         int i = 0;
-        mostrarDesglose(cambio, denominaciones, i);
+        mostrarDesglose(cambio, BilletesyMonedas, i);
     }
 
-    public static void mostrarDesglose(int cambio, int[] denominaciones, int i) {
-        if (i < denominaciones.length) {
-            int cantidad = cambio / denominaciones[i];
-            cantidad = (cambio % denominaciones[i] / denominaciones[i]) * cantidad
-                    + (1 - cambio % denominaciones[i] / denominaciones[i]) * cantidad;
-            cambio = cambio - cantidad * denominaciones[i];
-            System.out.println("Billete/Moneda de " + denominaciones[i] + " euros: " + cantidad);
-            i++;
-            mostrarDesglose(cambio, denominaciones, i);
-        }
+    public static void mostrarDesglose(int cambio, int[] BilletesyMonedas, int i) {
+        int cantidad = cambio / BilletesyMonedas[i];
+        cantidad = (cambio % BilletesyMonedas[i] / BilletesyMonedas[i]) * cantidad
+                + (1 - cambio % BilletesyMonedas[i] / BilletesyMonedas[i]) * cantidad;
+        cambio = cambio - cantidad * BilletesyMonedas[i];
+        System.out.println("Billete/Moneda de " + BilletesyMonedas[i] + " euros: " + cantidad);
+        i++;
+
+        mostrarDesglose(cambio, BilletesyMonedas, i);
     }
 }
