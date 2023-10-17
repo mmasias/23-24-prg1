34adbb63a8653fb7a71526b28e4c43b9bb8261d6
Daniel Lavín Aguado <114480268+LavinAguado@users.noreply.github.com>
2023-10-17 18:55:18 +0200

    Add files via upload

--- entregas/lavinDaniel/reto002/reto-006/Main.java
+++ entregas/lavinDaniel/reto002/reto-006/Main.java
@@ -4,13 +4,139 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int numeroPensado = 0;
+        int tarjetaActual = 1;
 
         System.out.println("Piensa un número entre el 1 y el 99.");
+        mostrarTarjeta(tarjetaActual);
 
-        for (int i = 1; i <= 10; i++) {
-            mostrarTarjeta(i);
+        if (tarjetaActual < 10) {
+            System.out.print("¿Está aquí? (1: SI / 2: NO): ");
+            int respuesta = input.nextInt();
 
-            System.out.print("¿Está aquí? (1:SI / 2:NO): ");
+            if (respuesta == 1) {
+                System.out.print("Ingresa el primer número que aparece en la tarjeta: ");
+                int numeroTarjeta = input.nextInt();
+                numeroPensado += numeroTarjeta;
+            }
+        }
+
+        tarjetaActual++;
+        mostrarTarjeta(tarjetaActual);
+
+        if (tarjetaActual < 10) {
+            System.out.print("¿Está aquí? (1: SI / 2: NO): ");
+            int respuesta = input.nextInt();
+
+            if (respuesta == 1) {
+                System.out.print("Ingresa el primer número que aparece en la tarjeta: ");
+                int numeroTarjeta = input.nextInt();
+                numeroPensado += numeroTarjeta;
+            }
+        }
+
+        tarjetaActual++;
+        mostrarTarjeta(tarjetaActual);
+
+        if (tarjetaActual < 10) {
+            System.out.print("¿Está aquí? (1: SI / 2: NO): ");
+            int respuesta = input.nextInt();
+
+            if (respuesta == 1) {
+                System.out.print("Ingresa el primer número que aparece en la tarjeta: ");
+                int numeroTarjeta = input.nextInt();
+                numeroPensado += numeroTarjeta;
+            }
+        }
+
+        tarjetaActual++;
+        mostrarTarjeta(tarjetaActual);
+
+        if (tarjetaActual < 10) {
+            System.out.print("¿Está aquí? (1: SI / 2: NO): ");
+            int respuesta = input.nextInt();
+
+            if (respuesta == 1) {
+                System.out.print("Ingresa el primer número que aparece en la tarjeta: ");
+                int numeroTarjeta = input.nextInt();
+                numeroPensado += numeroTarjeta;
+            }
+        }
+
+        tarjetaActual++;
+        mostrarTarjeta(tarjetaActual);
+
+        if (tarjetaActual < 10) {
+            System.out.print("¿Está aquí? (1: SI / 2: NO): ");
+            int respuesta = input.nextInt();
+
+            if (respuesta == 1) {
+                System.out.print("Ingresa el primer número que aparece en la tarjeta: ");
+                int numeroTarjeta = input.nextInt();
+                numeroPensado += numeroTarjeta;
+            }
+        }
+
+        tarjetaActual++;
+        mostrarTarjeta(tarjetaActual);
+
+        if (tarjetaActual < 10) {
+            System.out.print("¿Está aquí? (1: SI / 2: NO): ");
+            int respuesta = input.nextInt();
+
+            if (respuesta == 1) {
+                System.out.print("Ingresa el primer número que aparece en la tarjeta: ");
+                int numeroTarjeta = input.nextInt();
+                numeroPensado += numeroTarjeta;
+            }
+        }
+
+        tarjetaActual++;
+        mostrarTarjeta(tarjetaActual);
+
+        if (tarjetaActual < 10) {
+            System.out.print("¿Está aquí? (1: SI / 2: NO): ");
+            int respuesta = input.nextInt();
+
+            if (respuesta == 1) {
+                System.out.print("Ingresa el primer número que aparece en la tarjeta: ");
+                int numeroTarjeta = input.nextInt();
+                numeroPensado += numeroTarjeta;
+            }
+        }
+
+        tarjetaActual++;
+        mostrarTarjeta(tarjetaActual);
+
+        if (tarjetaActual < 10) {
+            System.out.print("¿Está aquí? (1: SI / 2: NO): ");
+            int respuesta = input.nextInt();
+
+            if (respuesta == 1) {
+                System.out.print("Ingresa el primer número que aparece en la tarjeta: ");
+                int numeroTarjeta = input.nextInt();
+                numeroPensado += numeroTarjeta;
+            }
+        }
+
+        tarjetaActual++;
+        mostrarTarjeta(tarjetaActual);
+
+        if (tarjetaActual < 10) {
+            System.out.print("¿Está aquí? (1: SI / 2: NO): ");
+            int respuesta = input.nextInt();
+
+            if (respuesta == 1) {
+                System.out.print("Ingresa el primer número que aparece en la tarjeta: ");
+                int numeroTarjeta = input.nextInt();
+                numeroPensado += numeroTarjeta;
+            }
+        }
+
+        tarjetaActual++;
+        mostrarTarjeta(tarjetaActual);
+
+        if (tarjetaActual < 10) {
+            System.out.print("¿Está aquí? (1: SI / 2: NO): ");
             int respuesta = input.nextInt();
 
             if (respuesta == 1) {
@@ -26,13 +152,6 @@ public class Main {
     public static void mostrarTarjeta(int numeroTarjeta) {
         System.out.println("TARJETA " + numeroTarjeta);
         System.out.println("+---------------+");
-        for (int i = 0; i < 10; i++) {
-            for (int j = 1; j <= 5; j++) {
-                int numero = (int) (Math.random() * 99) + 1;
-                System.out.print(String.format("| %2d ", numero));
-            }
-            System.out.println("|");
-        }
-        System.out.println("+---------------+");
+
     }
 }
