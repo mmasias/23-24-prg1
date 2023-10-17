5f726bedd7c4f5fe88e2bcd297311bc54f03f6b5
eirik.rosete <eirik.rosete@gmail.com>
2023-10-17 13:12:23 +0200

    reto006 en proceso

--- entregas/roseteEirik/reto006/006-AdivinarNumero.java
+++ entregas/roseteEirik/reto006/006-AdivinarNumero.java
@@ -4,6 +4,6 @@ class AdivinarNumero{
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
 
-        
+        final String PREGUNTA = "Dime (1 SI /)"
     }
 }
