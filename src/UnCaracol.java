d14a8c34a63db6e45478c188ebd93cc7c3ebb894
manuel@AIOlinux <manuel@masiasweb.com>
2023-10-24 14:24:55 +0200

    🐌

--- src/UnCaracol.java
+++ src/UnCaracol.java
@@ -9,7 +9,7 @@ class UnCaracol {
         int dia = 0;
         boolean estaDentro = true;
         boolean estaVivo = true;
-        System.out.println("El dia [" + dia + "] el caracol cayo hasta [" + profundidadCaracol + "]m");
+        System.out.println("El dia [" + dia + "] el caracol cayo hasta [" + profundidadCaracol + "] m");
 
         double subidaMaxima = 4.0;
         double subidaMinima = 1.0;
@@ -21,23 +21,22 @@ class UnCaracol {
             dia++;
             System.out.println("Dia " + dia);
 
-            double caracolSube = Math.random()*(subidaMaxima-subidaMinima)+subidaMinima;
+            double caracolSube = Math.random() * (subidaMaxima - subidaMinima) + subidaMinima;
             System.out.println("El caracol sube: " + caracolSube);
 
             profundidadCaracol = profundidadCaracol - caracolSube;
+            
+            if (profundidadCaracol < 0) {
+                estaDentro = false;
+            }
 
-            double caracolBaja = Math.random()*(bajadaMaxima-bajadaMinima)+bajadaMinima;
+            double caracolBaja = Math.random() * (bajadaMaxima - bajadaMinima) + bajadaMinima;
             System.out.println("El caracol baja: " + caracolBaja);
             profundidadCaracol = profundidadCaracol + caracolBaja;
 
             System.out.println("Al final del dia está en " + profundidadCaracol);
 
             inputUsuario = scanner.nextLine();
-
-            if (profundidadCaracol<0) {
-                estaDentro = false;
-            }
-
 
             if (dia == 50) {
                 estaVivo = false;
@@ -55,7 +54,7 @@ class UnCaracol {
             estadoCaracol = estadoCaracol + " y ha salido";
         } else {
             estadoCaracol = estadoCaracol + " y no ha salido";
-        }        
+        }
         System.out.println("El caracol al final esta " + estadoCaracol);
 
     }
