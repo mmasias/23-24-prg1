44c9abb6c76e494dd0d879f82b31ee601cd7b99c
eirik.rosete <eirik.rosete@unealum.local>
2023-10-24 14:03:43 +0200

    avance del caracol

--- entregas/roseteEirik/retoCaracol/ElCaracol.java
+++ entregas/roseteEirik/retoCaracol/ElCaracol.java
@@ -5,15 +5,44 @@ class ElCaracol{
         
         Scanner scanner = new Scanner(System.in);
 
-        int deepSnail = (int) (Math.random() * 10) + 10;
+        double deepSnail = (double) (Math.random() * 10) + 10;
         int day = 0;
         boolean snailIn = true;
         boolean snailAlive = true;
+        String inputUser;
 
-        System.out.println("El dìa [" + day + "] el caracol cayo hasta [" + deepSnail + "]");
+        System.out.println("El dia [" + day + "] el caracol cayo hasta [" + deepSnail + "] metros");
+
+        double goingUpMax = 4;
+        double goingUpMin = 1;
+
+        double goingDownMax = 2;
+        double goingDownMin = 0;
 
         while(snailIn && snailAlive){
             day++;
+            System.out.println("\nDia " + day);
+
+            double snailGoingUp = (double)(Math.random()*(goingUpMax - goingUpMin) + goingUpMin);
+            double snailGoingDown = (double)(Math.random()*(goingDownMax - goingDownMin));
+
+            deepSnail = deepSnail + snailGoingUp - snailGoingDown;
+
+            System.out.println("El caracol subio [" + snailGoingUp + "] metros\n");
+                ("El caracol se encuentra en [" + deepSnail + "] metros");
+
+            inputUser = scanner.nextLine();
+
+            if (deepSnail >= 20){
+                snailIn = false;
+                System.out.println("El caracol ha salido del pozo!");
+            else
+                ("El caracol cayo [" + snailGoingDown + "] metros\n");
+            }
+            if(day == 50){
+                snailAlive = false;
+                System.out.println("El caracol ha muerto");
+            }
         }
 
     }
