738253752d1f03f293f244e15e3e805d9485feec
eirik.rosete <eirik.rosete@gmail.com>
2023-11-06 17:19:42 +0100

    Practicando para el examen final

--- src/roseteEirik/VirusGame/v001/FirstTryVirusGame.java
+++ src/roseteEirik/VirusGame/v001/FirstTryVirusGame.java
@@ -1,4 +1,14 @@
 package roseteEirik.VirusGame.v001;
 import java.util.Scanner;
 
+class FirstTryVirus{
+    public static void main(String[] args){
 
+        Scanner scanner = new Scanner(System.in);
+
+
+
+        
+
+    }
+}
