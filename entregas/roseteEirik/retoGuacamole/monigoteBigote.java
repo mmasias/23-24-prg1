5c3a72f5c79e582610cc573e14796f8e4d7fe0fd
eirik.rosete <eirik.rosete@gmail.com>
2023-11-26 20:48:54 +0100

    Reto de Whac-A-Mole finalizado (incluidos retos extendidos)

--- entregas/roseteEirik/retoGuacamole/monigoteBigote.java
+++ entregas/roseteEirik/retoGuacamole/monigoteBigote.java
@@ -1,20 +1,103 @@
 package roseteEirik.retoGuacamole;
 import java.util.Scanner;
 
-class monigoteBigote {
+class MonigoteBigote {
     public static void main(String[] args) {
         
         Scanner scanner = new Scanner(System.in);
+        String inputUser;
 
         final int MAX_TURNS = 15;
         int playerChoice = 0;
+        int totalHits = 0;
+    
+        int mole = 0;
+        int moleTwo = 0;
+
+        int moleLastPosition = 0;
+        int moleTwoLastPosition = 0;
+
+        final String DIVISION_NEXT_TURN = "=".repeat(30);
+        final String INTRO = "\n";
         String messageTurn = "";
+        String messageHitOrFail = "";
 
         for (int turn = 1; turn <= MAX_TURNS; turn++){
+
+            System.out.print("Golpe entre 1 y 16: ");
+            
             playerChoice = scanner.nextInt();
-            messageTurn = "Golpe en [" + playerChoice + "]";
+            System.out.print(cleanScreen());
+            
+            do {
+
+                do {
+                    mole = (int) (Math.random() * 16 + 1);
+                } while (mole == moleLastPosition);
+
+                do {
+                    moleTwo = (int) (Math.random() * 16 + 1);
+                } while (mole == moleTwo || moleTwoLastPosition == moleTwo);
+
+            } while (moleLastPosition == moleTwo || moleTwoLastPosition == mole);
+
+            if (playerChoice == mole || playerChoice == moleTwo){
+                totalHits++;
+                messageHitOrFail = "[ACIERTO]";
+            } else {
+                messageHitOrFail = "[FALLO]";
+            }
+            messageTurn = 
+                "Golpe en [" + playerChoice + "] / Monigotes en [" + mole + "," + moleTwo + "]" +
+                drawBoardGame(playerChoice, mole, moleTwo) + INTRO +
+                "Turno [" + turn + "] / Aciertos [" + totalHits + "]" + INTRO +
+                messageHitOrFail + INTRO +
+                DIVISION_NEXT_TURN;
             System.out.println(messageTurn);
+
+            moleLastPosition = mole;
+            moleTwoLastPosition = moleTwo;
         }    
+    }
+    static String drawBoardGame(int playerChoice, int mole, int moleTwo){
+        final int MAX_ROWS = 4;
+        final int MAX_COLUMNS = 4;
+
+        final String INTRO = "\n";
+        final String LINE = "-".repeat(28);
+        final String DIVISION = INTRO + "+" + LINE + "+";
+
+        final String EMPTY_BOX = "(  )  ";
+        final String MOLE = "('')  ";
+        final String SECOND_MOLE = "(**)  ";
+        final String HAMMER = "[[]]  "; 
+        final String HIT_MOLE = "[><]  ";
+        
+        String draw = DIVISION;
+        int number = 0;
+
+        for (int totalRows = 0; totalRows < MAX_ROWS; totalRows++){
+            draw = draw + INTRO + "|   ";
+            for (int totalColumns = 0; totalColumns < MAX_COLUMNS; totalColumns++){
+                number++;
+                if (number == mole && number == playerChoice){
+                    draw = draw + HIT_MOLE;
+                } else if (number == moleTwo && number == playerChoice){
+                    draw = draw + HIT_MOLE;
+                } else if (number == mole){
+                    draw = draw + MOLE;
+                } else if (number == moleTwo){
+                    draw = draw + SECOND_MOLE;
+                } else if (number == playerChoice){
+                    draw = draw + HAMMER;
+                } else {
+                    draw = draw + EMPTY_BOX;
+                }
+            }
+            draw = draw + " |";
+        }
+        draw = draw + DIVISION;
+        return draw;
     }
     static String cleanScreen(){
         return "\033[H\033[2J";
