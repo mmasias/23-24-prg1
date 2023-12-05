5d01950e43d580759d086ef6966e9a153a1d0c05
Cayetano Castillo⚽️ <114479626+MikaOT@users.noreply.github.com>
2023-12-05 23:19:12 +0100

    Update and rename generadorNumero.java to GeneradorNumero.java

--- entregas/cayetanoCastillo/Reto-004/GeneradorNumero.java
+++ entregas/cayetanoCastillo/Reto-004/GeneradorNumero.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class generadorNumero {
+public class GeneradorNumero {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
