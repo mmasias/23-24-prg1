ed4f54576cc1c92e69a28be09980081ae4205db2
eirik.rosete <eirik.rosete@gmail.com>
2023-12-11 09:13:36 +0100

    Ya corregido

--- entregas/roseteEirik/retos/ExamenParcial/OtroEdificio.java
+++ entregas/roseteEirik/retos/ExamenParcial/OtroEdificio.java
@@ -1,3 +1,5 @@
+package roseteEirik.retos.ExamenParcial;
+
 class OtroEdificio {
     public static void main(String[] args) {
 
--- entregas/roseteEirik/retos/ExamenParcial/OtroEdificioMejorado.java
+++ entregas/roseteEirik/retos/ExamenParcial/OtroEdificioMejorado.java
@@ -1,66 +1,67 @@
 package roseteEirik.retos.ExamenParcial;
 
-class OtroEdificioMejorado {
+class OtroEdificioMejorado{
     public static void main(String[] args) {
 
         String message = "";
         String energyConsumed = "";
         int energyAverage = 0;
-        
+
         final int DAYS_PER_WEEK = 7;
-        
         for(int day = 1; day <= DAYS_PER_WEEK; day++){
             String noEnergyMessage = "";
+
+            final int WINDOWS_PER_FLOOR = 6;
+            int damagedColumn = 0;
+            final int TOTAL_BUILDING_FLOORS = 7;
+            int floorUnderMaintenance = 0;
             
-            final int MAX_ROWS = 7;
-            int row = 0;
-            final int MAX_COLUMNS = 6;
-            int column = 0;
             int stadistic = 0;
             
-            boolean maintenanceProbability = Math.random() < 0.05;
-            
-            if(maintenanceProbability){
-                row = (int)(Math.random()*(MAX_ROWS)+1);
-                noEnergyMessage = "\nPlanta " + row + " en mantenimiento";
-            }
-            
-            final int MAX_HOURS_DAY = 23;
-            
-            for(int hour = 0; hour <= MAX_HOURS_DAY; hour++){
+            for(int hour = 0; hour <= 23; hour++){
                 
                 System.out.println(energyConsumed);
                 pause(3);
                 cleanScreen();
-                          
-                boolean lightningProbability = Math.random() < 0.20;
                 
-                if (column != 0){
-                    lightningProbability = false;
-                } else if(lightningProbability){
-                    column = (int)(Math.random()*(MAX_COLUMNS)+1);
-                    noEnergyMessage = noEnergyMessage + "\nUn rayo ha inutilizado la columna " + column;
+                final double LIGHTNING_STRIKE_PROBABILITY = 0.20;
+                boolean lightning = Math.random() < LIGHTNING_STRIKE_PROBABILITY;
+                if (damagedColumn != 0){
+                    lightning = false;
+                } else if(lightning){
+                    damagedColumn = (int)(Math.random()*(WINDOWS_PER_FLOOR)+1);
+                    noEnergyMessage = "\nUn rayo ha inutilizado la columna " + damagedColumn;
+                }
+                
+                final double MAINTENANCE_PROBABILITY = 0.05;
+                boolean maintenance = Math.random() < MAINTENANCE_PROBABILITY;
+                if (floorUnderMaintenance != 0){
+                    maintenance = false;
+                } else if(maintenance){
+                    floorUnderMaintenance = (int)(Math.random()*(TOTAL_BUILDING_FLOORS)+1);
+                    noEnergyMessage = noEnergyMessage + "\nPlanta " + floorUnderMaintenance + " en mantenimiento";
                 }
                 
                 int lightPerHour = 0;
-                final int BOTTOM_FLOOR = 1;
-
                 drawBuildingTop();
-                for(int floor = 7; floor >= BOTTOM_FLOOR; floor--){
-
-                    final int WINDOWS_PER_FLOOR = 6;
-
+                for(int floor = TOTAL_BUILDING_FLOORS; floor >= 1; floor--){
+                    
                     for (int window = 1; window <= WINDOWS_PER_FLOOR; window++){
-                        boolean blindOpen = Math.random() < 0.70;
-                        boolean lightOn = Math.random() < 0.60;
-                        if(column == window){
-                            System.out.print(":[X]:");
-                        } else if (row == floor){
-                            System.out.print(":[#]:");
-                        } else {
+
+                        final double BLIND_OPEN_PROBABILITY = 0.70;
+                        boolean blindOpen = Math.random() < BLIND_OPEN_PROBABILITY;
+
+                        final double LIGHT_ON_PROBABILITY = 0.60;
+                        boolean lightOn = Math.random() < LIGHT_ON_PROBABILITY;
+
+                        boolean windowDamagedLight = window == damagedColumn;
+                        boolean windowUnderMaintenance = floor == floorUnderMaintenance;
+
+                        if(!(windowDamagedLight || windowUnderMaintenance)){
                             if (lightOn){lightPerHour++;}
-                            windowDraw(blindOpen, lightOn, lightningProbability);
                         }
+                        
+                        windowDraw(blindOpen, lightOn, windowDamagedLight, windowUnderMaintenance);
 
                         boolean elevator = 3 == window % 6;
                         System.out.print(elevator ? "[    ]":"");
@@ -96,10 +97,14 @@ class OtroEdificioMejorado {
         System.out.println(TOP);
     }
 
-    static void windowDraw(boolean blindOpen, boolean lightOn, boolean lightning){
+    static void windowDraw(boolean blindOpen, boolean lightOn, boolean windowDamagedLight, boolean windowUnderMaintenance){
         String windowDraw = ":[ ]:";
         
-        if (blindOpen && lightOn){
+        if (windowUnderMaintenance){
+            windowDraw = ":[#]:";
+        } else if (windowDamagedLight){
+            windowDraw = ":[X]:";
+        } else if (blindOpen && lightOn){
             windowDraw = ":[*]:";
         } else if (blindOpen){
             windowDraw = ":[']:";
