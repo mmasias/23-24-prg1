f0877c999c8ae53fa6278cfc3450010a5ee0b7f0
eirik.rosete <eirik.rosete@gmail.com>
2024-01-18 17:40:36 +0100

    Todo en orden antes de empezar a practicar para el final

--- entregas/roseteEirik/retos/retoLimpieza/limpiezaOficina.java
+++ entregas/roseteEirik/retos/retoLimpieza/limpiezaOficina.java
@@ -32,6 +32,7 @@ class limpiezaOficina {
             if(Math.random()<.20){rowVacuum = rowVacuum+((Math.random()<.50)?1:-1);}
             if(Math.random()<.70){columnVacuum = columnVacuum+((Math.random()<.50)?1:-1);}
             renderWorld(theOffice, rowVacuum, columnVacuum);
+            showStatus(theOffice, rowVacuum, columnVacuum);
             System.out.println("Tiempo: " + time);
         }
 
@@ -39,10 +40,13 @@ class limpiezaOficina {
 
     private static void renderWorld(int[][] theOffice, int rowVacuum, int columnVacuum){
         final int VACUUM_TILE = 6;
-
+        final int LINE_TILE = 7;
+        
         pause(1);
         cleanScreen();
+        System.out.println(showTile(LINE_TILE).repeat(theOffice[0].length + 2));
         for (int row = 0; row<theOffice.length; row ++){
+            System.out.print("|| ");
             for (int column = 0; column<theOffice[row].length; column ++){
                 if (row == rowVacuum && column == columnVacuum){
                     System.out.print(showTile(VACUUM_TILE));
@@ -51,13 +55,22 @@ class limpiezaOficina {
                     System.out.print(showTile(theOffice[row][column]));
                 }
             }
+            System.out.print(" ||");
             System.out.println();
         }
-        System.out.println("\nAspiradora en [" + rowVacuum + "][" + columnVacuum + "] | Suciedad detectada: " + showTile(theOffice[rowVacuum][columnVacuum]));
+    }
+    
+    private static void showStatus(int[][] theOffice, int rowVacuum, int columnVacuum) {
+        final int LINE_TILE = 7;
+        
+        System.out.println(showTile(LINE_TILE).repeat(theOffice[0].length + 2));
+        System.out.println("Mundo de ["+ theOffice.length +"]["+ theOffice[0].length +"]");
+        System.out.println("Aspiradora en [" + rowVacuum + "][" + columnVacuum + "] | Suciedad detectada: " + showTile(theOffice[rowVacuum][columnVacuum]));
+        System.out.println(showTile(LINE_TILE).repeat(theOffice[0].length + 2));
     }
 
     private static String showTile(int tile) {
-        String[] tiles = {" . ","...","ooo","OOO","XXX","***","(0)"}; 
+        String[] tiles = {" . ","...","ooo","OOO","XXX","***","(0)", "==="}; 
         return tiles[tile];
     }
     
