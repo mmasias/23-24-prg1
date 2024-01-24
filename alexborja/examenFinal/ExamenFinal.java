e5e11c00af0f66cf7f60fe8d590894844011d311
alexborja <gersonperezgerder@gmail.com>
2024-01-24 13:15:09 +0100

    hadle option 1 and working on option 2

--- alexborja/examenFinal/ExamenFinal.java
+++ alexborja/examenFinal/ExamenFinal.java
@@ -2,7 +2,28 @@ import java.util.Scanner;
 
 class ExamenFinal {
 	public static void main(String[] args) {
-		String[] elements = { "Aire", "Agua", "Tierra", "Fuego" };
+		String[][] elements = { 
+			{ "Aire" }, { "Agua" } , { "Tierra "} ,{ "Fuego" }, 
+			{ "Agua", "Fuego", "Vapor"}, { "Agua", "Tierra", "Barro"}, 
+			{ "Fuego", "Tierra", "Lava"}, { "Aire", "Tierra", "Polvo"}, 
+			{ "Agua", "Aire", "Lluvia"},  { "Barro", "Lluvia", "Planta" },
+			{ "Fuego", "Lava", "Hierro" }, { "Aire", "Lava", "Arena" },
+			{ "Arena", "Fuego", "Vidrio" }, { "Hierro", "Vapor", "Alcohol" },
+			{ "Barro", "Energia", "Vida"  }, { "Aire", "Fuego", "Energía" },
+			{ "Energía", "Lluvia", "Tormenta" }, { "Barro", "Fuego", "Metal" },
+			{ "Energía", "Metal", "Electricidad"}, { "Agua", "Electricidad", "Oxígeno"},
+			{ "Electricidad", "Aire", "Ozono" }, { "Carbono", "Presión", "Diamante"},
+			{ "Planta", "Tierra", "Carbono" }, { "Tierra" , "Tierra", "Presión",},
+			{ "Planta", "Metal", "Madera" }, { "Hierro", "Madera", "Herramienta" },
+			{ "Herramienta", "Vapor", "Vaporizador" }, { "Metal", "Electricidad", "Computadora"},
+			{ "Computadora", "Energía", "Robot" }
+		};
+		String[] userElements = new String[29];
+		userElements[0] = "Aire";
+		userElements[1] = "Agua";
+		userElements[2] = "Tierra";
+		userElements[3] = "Fuego";
+
 		boolean exit = false;
 
 		gretingMessage();
@@ -11,7 +32,7 @@ class ExamenFinal {
 			int userOption = askUserInput();
 			exit = userOption == 3 ? true : exit;
 
-			optionHandler(userOption, elements);
+			optionHandler(userOption, userElements, elements);
 		}
 		System.out.println("\nPrograma finalizado!");
 	}
@@ -45,22 +66,58 @@ class ExamenFinal {
 		return userOption;
 	}
 
-	static void optionHandler(int userOption, String[] elements) {
+	static void optionHandler(int userOption, String[] userElements, String[][] elements) {
 		if(userOption == 1) {
-			System.out.println("Elementos conocidos:\n");
-			for(int i = 0; i < elements.length; i++) {
-				System.out.println(elements[i]);
-			}
-			System.out.println();
+			showElements(userElements);
 		}
 
 		if(userOption == 2) {
-			System.out.println("option 1");
+			createElements(userElements, elements)
 		}
 
 		if(userOption == 3) {
 			System.out.println("option 1");
 		}
 	}
+
+	static void showElements(String[] userElements) {
+		System.out.println("Elementos conocidos:\n");
+		for(int i = 0; i < userElements.length; i++) {
+			if(userElements[i] != null) {
+				System.out.println((i + 1) + ". " + userElements[i]);
+			}
+		}
+		System.out.println();
+	}
+
+	static void createElement(String[] userElements, String[] elements) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Selecciona dos elementos para combinarlos.");
+
+		System.out.print("Ingresa el número del primer elemento: ");
+		int elementOne = scanner.nextInt();
+
+		System.out.print("Ingresa el número del segundo elemento: ");
+		int elementTwo = scanner.nextInt();
+
+		String ele1 = userElements[elementOne];
+		String ele2 = userElements[elementTwo];
+
+		for(int i = 0; i < elements.length; i++) {
+			String[] currentElement = elements[i];
+			String[] toMatch = { currentElement[0], currentElement[1] };
+			
+
+		}
+	}
 }
 
+/*
+ *	["agua", "aire"] -> user
+ *	[row[0], row[1]] -> elements
+ *
+ *  user[0] in elements && user[1] in elements ?
+ *  element = elements[i][2];
+ *  add userElements[i] = element
+ *
+ * */
